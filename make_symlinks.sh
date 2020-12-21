5a7c80446c7aa36a216f9cd3370e36f75062473f
LcitsChan <ctairong@gmail.com>
2020-12-21 11:57:02 +0800

    link: manage link script manually

---
+++ make_symlinks.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DOTDIR="$(pwd)"
+BACKUPDIR=$HOME/.backup
+if [ ! -e $BACKUPDIR ]; then
+  echo $BACKUPDIR
+  mkdir -p $BACKUPDIR
+fi
+
+make_link() {
+  echo "==="
+  if [ ! -e $1 ]; then
+    echo "$1 not exit"
+    echo "===\n"
+    return
+  fi
+  ldd=$(dirname $2)
+  if [ ! -d $ldd ]; then
+    mkdir -p $ldd
+  fi
+  if [ -e $2 ]; then
+    bk_date="$(date '+%Y%m%d%H%M%S')"
+    bk_name="${BACKUPDIR}/$(basename "$2").${bk_date}"
+    $(
+    set -x;
+    mv $2 $bk_name
+    )
+  fi 
+  $(
+  set -x;
+  ln -sfn $1 $2
+  )
+  echo "==="
+}
+
+make_link $DOTDIR/nvim $HOME/.config/nvim
+make_link $DOTDIR/ranger $HOME/.config/ranger
+make_link $DOTDIR/tmux/.tmux.conf $HOME/.tmux.conf
+make_link $DOTDIR/zsh/.zshrc $HOME/.zshrc
