a6bc559d339fa193bd8c356b9b3e7111b6329e6f
LcitsChan <ctairong@gmail.com>
2021-03-03 22:52:12 +0800

    chore: install docker script on ubuntu

---
+++ install/install-docker.sh
@@ -0,0 +1,10 @@
+case `uname -a` in
+  *Ubuntu*)
+  sudo apt -q update
+  sudo apt install -q -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  sudo apt update
+  sudo apt install docker-ce
+  ;;
+esac
